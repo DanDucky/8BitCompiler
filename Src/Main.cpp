5224715508e8c19fc0458dd5aa25f77d2db89cbb
DanDucky <holdenwellsbennett@gmail.com>
2022-08-07 12:56:37 -0700

    Command Line Visuals
    + All visuals in command line
    + Working timer
    + Counts compiled lines
    
    Also I can now confidently say that the code is TWICE as fast as the previous compiler! (if not even faster in some cases)

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -11,20 +11,48 @@ void FileWrite(ofstream &OutputFile, string Binary) {
 }
 
 void FileRead(char** argv) {
+	int LineNumOut = 1;
 	ifstream InputFile;
 	InputFile.open(argv[1]);
 	string Line;
+	string Binary;
 	ofstream OutputFile;
 	OutputFile.open((argv[2]));
+	int Arrow;
 	if (InputFile.is_open()) {
 		int LineNum = 0;
 		while (getline(InputFile, Line)) {
-			auto start = high_resolution_clock::now();
 			LineNum++;
-			FileWrite(OutputFile, InstructionP(Line));
+			cout << "Line " << LineNum << " ┼ " << Line << " ";
+			auto start = high_resolution_clock::now();
+			Binary = InstructionP(Line);
+			FileWrite(OutputFile, Binary);
 			auto stop = high_resolution_clock::now();
 			auto Duration = duration_cast < microseconds > (stop - start);
-			cout << Duration.count() + "microseconds\n";
+			for (Arrow = 0; Arrow < 24 - Line.length(); Arrow++) {
+				cout << "─";
+			}
+			if (Line.substr(0, 4) == "MOVE" || Line.substr(0, 4) == "DECL") {
+				cout << "┬─" << "> " << Binary.substr(0, 8)
+						<< " ┼ Compiled line "
+						<< LineNumOut
+						<< " ─╮\n       │                          ╰─> "
+						<< Binary.substr(9, 8);
+				LineNumOut++;
+				cout << " ┼ Compiled line " << LineNumOut
+						<< " ─┴─> \033[1;32mcompleted\033[0m in "
+						<< Duration.count()
+						<< " microseconds\n";
+				LineNumOut++;
+			} else {
+				cout << "──> " << Binary
+						<< " ┼ Compiled line " << LineNumOut
+						<< " ───> \033[1;32mcompleted\033[0m in "
+						<< Duration.count()
+						<< " microseconds\n";
+				LineNumOut++;
+			}
+			
 		}
 
 	} else {
@@ -33,5 +61,8 @@ void FileRead(char** argv) {
 }
 
 int main (int argc, char** argv) {
+//	cout << "─┬─>";
+//           └└─>
+	cout << "Compiling " << argv[1] << " to " << argv[2] << "\n";
 	FileRead(argv);
 }
